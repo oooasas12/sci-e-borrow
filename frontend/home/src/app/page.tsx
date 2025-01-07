a4a2fa2f9819829c6debab2b0fc2812eaafe1a81
Donut <oooasas12@gmail.com>
2025-01-07 07:59:54 +0700

    index

--- frontend/home/src/app/page.tsx
+++ frontend/home/src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.location.href = '/login'
+  }, [])
   return (
     <div>
-      <h1 className="text-4xl">TEST</h1>
     </div>
   );
 }
